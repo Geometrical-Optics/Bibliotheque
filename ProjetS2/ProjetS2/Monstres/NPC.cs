e8f81826cd1c57020c158e2074e35a582be3c82d
Ryan <ryan.dahout@epita.fr>
2023-03-07 20:32:50 +0100

    oui

---
+++ ProjetS2/ProjetS2/Monstres/NPC.cs
@@ -0,0 +1,25 @@
+using ProjetS2.Map;
+
+namespace ProjetS2;
+
+
+public abstract class NPC 
+{
+    public string Symbol;
+    public int Id;
+    public Box[,] Board;
+    public (double X, double Y) Coordinates;
+
+    public NPC(int id, (double X, double Y) coordinates, Box[,] board)
+    {
+        
+        Id = id;
+        Coordinates = coordinates;
+        Board = board;
+    }
+    
+    public override string ToString()
+    {
+        return Symbol;
+    }
+}
