4ffa1c1039607740623ea3a79fd64c39b8d834ea
Fedaouche Yvan <yvan.fedaouche@epita.fr>
2023-04-15 19:48:33 +0200

    1er commit

---
+++ MoteurRendu/Raycaster/Raycaster/Box/Full.cs
@@ -0,0 +1,57 @@
+﻿namespace Raycaster;
+
+public class Cylinder : Box
+{
+    public (int X, int Y) _Coordinates { get; private set; }
+    public (double X, double Y)[] _Vertex { get; private set; }
+    public float _Height { get; private set; }
+    public int _TextureId { get; private set; }
+    public float _Size { get; private set; }
+    public int _FloorId { get; private set; }
+    public int _CeilingId { get; private set; }
+    
+    public int _TopDownId { get; private set; }
+
+    public bool _IsTransparent { get; private set; }
+    public bool _ContainsEntity { get; set; }
+    public float _posZ { get; private set; }
+
+    public Cylinder((int X, int Y) Coordinates, float Height, int TextureId, int floor, 
+        int ceil, int topid, float pos_Z, float size)
+    {
+        _Coordinates = Coordinates;
+        _Height = Height;
+        _TextureId = TextureId;
+        _Vertex = new (double X, double Y)[1]{Coordinates};
+        _Size = size;
+        _FloorId = floor;
+        _CeilingId = ceil;
+        _TopDownId = topid;
+
+        if (Height == 1 && pos_Z == 0)
+            _IsTransparent = false;
+        else
+            _IsTransparent = true;
+
+        _posZ = pos_Z;
+        _ContainsEntity = false;
+    }
+
+    public bool IsColliding((double X, double Y) Coordinates)
+    {
+        if (Math.Sqrt((Coordinates.X-_Coordinates.X-0.5)*(Coordinates.X-_Coordinates.X-0.5) 
+                      + (Coordinates.Y-_Coordinates.Y-0.5)*(Coordinates.Y-_Coordinates.Y-0.5)) < _Size)
+            return true;
+
+        return false;
+    } 
+    
+    public bool Collide((double X, double Y, float Z) Coordinates)
+    {
+        if (((int)Coordinates.X == _Coordinates.X && (int)Coordinates.Y == _Coordinates.Y)
+            && (Coordinates.Z >= _posZ-0.75 && Coordinates.Z < _posZ+_Height ))
+            return true;
+
+        return false;
+    }
+}
---
+++ MoteurRendu/Raycaster/Raycaster/RaycastSprite.cs
@@ -0,0 +1,35 @@
+﻿using SFML.Graphics;
+
+namespace Raycaster;
+
+public class RaycastSprite
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+
+    public (float X, float Y, float Z) _Size { get; set; }
+    public Image _Texture { get; }  
+    public (float X, float Y, float Z) _TexturePosition { get; set; }
+
+    public (float X, float Y, float Z) _Position
+    {
+        get { return (X, Y, Z); }
+        set {
+            X = value.X;
+            Y = value.Y;
+            Z = value.Z;
+        }
+    }
+
+    public RaycastSprite((float X, float Y, float Z) Position, (float X, float Y, float Z) Size, Image Texture, (float X, float Y, float Z) TexturePosition)
+    {
+        X = Position.X;
+        Y = Position.Y;
+        Z = Position.Z;
+
+        _Size = Size;
+        _Texture = Texture;
+        _TexturePosition = TexturePosition;
+    }
+}
