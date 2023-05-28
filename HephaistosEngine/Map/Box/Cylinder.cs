fc3a16fd9099ba4742c7beb116f0ba1091462512
Fedaouche Yvan <yvan.fedaouche@epita.fr>
2023-05-28 14:00:19 +0200

    Push avec Map

---
+++ HephaistosEngine/Map/Box/Cylinder.cs
@@ -0,0 +1,150 @@
+﻿using System.Net.Mime;
+using SFML.Graphics;
+
+namespace Map;
+
+public class Cylinder : Box
+{
+    public (int X, int Y) _Coordinates { get; private set; }
+    public (float X, float Y) _TexturePos { get; set; }
+
+    public (double X, double Y)[] _Vertex { get; private set; }
+    public float _Height { get; private set; }
+    public int _TextureId { get; private set; }
+    public float _Size { get; private set; }
+    public float _Size2 { get; private set; }
+    public int _FloorId { get; private set; }
+    public int _CeilingId { get; private set; }
+    
+    public int _TopDownId { get; private set; }
+
+    public bool _IsTransparent { get; private set; }
+    public bool _ContainsEntity { get; set; }
+    public float _posZ { get; private set; }
+
+    public Cylinder((int X, int Y) Coordinates, float Height, int TextureId, int floor, 
+        int ceil, int topid, float pos_Z, float size)
+    {
+        _Coordinates = Coordinates;
+        _Height = Height;
+        _TextureId = TextureId;
+        _Vertex = new (double X, double Y)[1]{Coordinates};
+        _Size = size*size;
+        _Size2 = size;
+        _FloorId = floor;
+        _CeilingId = ceil;
+        _TopDownId = topid;
+        _TexturePos = (0, 0);
+
+        if (Height == 1 && pos_Z == 0)
+            _IsTransparent = false;
+        else
+            _IsTransparent = true;
+
+        _posZ = pos_Z;
+        _ContainsEntity = false;
+    }
+
+    public bool IsColliding((double X, double Y) Coordinates)
+    {
+        if (((Coordinates.X-_Coordinates.X-0.5)*(Coordinates.X-_Coordinates.X-0.5) 
+                      + (Coordinates.Y-_Coordinates.Y-0.5)*(Coordinates.Y-_Coordinates.Y-0.5)) < _Size)
+            return true;
+
+        return false;
+    } 
+    
+    public bool Collide((double X, double Y, float Z) Coordinates)
+    {
+        if (((int)Coordinates.X == _Coordinates.X && (int)Coordinates.Y == _Coordinates.Y)
+            && (Coordinates.Z >= _posZ-0.75 && Coordinates.Z < _posZ+_Height ))
+            return true;
+
+        return false;
+    }
+    
+    public void Save(string path, BinaryWriter sw)
+    {
+        sw.Write(2);
+        sw.Write(_Coordinates.X);
+        sw.Write(_Coordinates.Y);
+        sw.Write((double)_posZ);
+        
+        sw.Write((double)_TexturePos.X);
+        sw.Write((double)_TexturePos.Y);
+        
+        sw.Write(_TextureId);
+        sw.Write(_FloorId);
+        sw.Write(_CeilingId);
+        sw.Write(_TopDownId);
+        sw.Write((double)_Height);
+        sw.Write((double)_Size);
+    }
+    
+    public static Box Read(string path, BinaryReader sr)
+    {
+        int x;
+        int y;
+        double z;
+        
+        double x1;
+        double y1;
+        
+        int text;
+        int floor;
+        int ceil;
+        int top;
+        double height;
+        double size;
+        
+        x = sr.ReadInt32();
+        y = sr.ReadInt32();
+        z = sr.ReadDouble();
+        
+        x1 = sr.ReadDouble();
+        y1 = sr.ReadDouble();
+        
+        text = sr.ReadInt32();
+        floor = sr.ReadInt32();
+        ceil = sr.ReadInt32();
+        top = sr.ReadInt32();
+        height = sr.ReadDouble();
+        size = sr.ReadDouble();
+
+        var tmp = new Cylinder((x, y), (float)height, text, floor, ceil, top, (float)z, (float)size);
+
+        tmp._TexturePos = ((float)x1, (float)y1);
+
+        return tmp;
+    }
+    
+    public uint GetTextureX((double X, double Y) Coordinates, Image Texture)
+    {
+        uint xx = (uint)(((Coordinates.X+_TexturePos.X) % 1) * (Texture.Size.X - 1));
+
+        if ((Coordinates.X % 1) < _Size2/2 || (Coordinates.X % 1)+_Size2/2 > 1)
+        {
+            xx = (uint)(((Coordinates.Y+_TexturePos.Y) % 1) * (Texture.Size.X - 1));
+        }
+
+        return xx;
+    }
+
+    public bool Same(Box tmp)
+    {
+        if (tmp._Coordinates == _Coordinates
+            && tmp._Height == _Height
+            && tmp._posZ == _posZ
+            && tmp._Vertex == _Vertex)
+            return true;
+
+        return false;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Box)
+            return Same((Box)(obj));
+        return false;
+    }
+}
---
+++ HephaistosEngine/Map/Cartes.cs
@@ -0,0 +1,187 @@
+﻿using System.Text;
+using Raycasting;
+
+namespace Map;
+
+public class Carte
+{
+    public Chunck[,] _Carte { get; private set; }
+    public int _Height { get; private set; }
+    public int _Width { get; private set; }
+    public int _Depth { get; private set; }
+    private int _ChunkSize;
+    
+
+    public List<Box> this[int x, int y]
+    {
+        get
+        {
+            return _Carte[x / _ChunkSize, y / _ChunkSize][x % _ChunkSize, y % _ChunkSize];
+        }
+        set
+        {
+            _Carte[(int)(x / _ChunkSize), (int)(y / _ChunkSize)][x % _ChunkSize, y % _ChunkSize] = value;
+        }
+    }
+    
+    public Box this[int x, int y, int z]
+    {
+        get
+        {
+            return _Carte[x / _ChunkSize, y / _ChunkSize][x % _ChunkSize, y % _ChunkSize, z];
+        }
+        set
+        {
+            _Carte[(int)(x / _ChunkSize), (int)(y / _ChunkSize)][x % _ChunkSize, y % _ChunkSize, z] = value;
+        }
+    }
+
+    public Carte((int Width, int Height) Size, int ChunkSize)
+    {
+        _Height = Size.Height;
+        _Width = Size.Width;
+        _Depth = 1;
+        _ChunkSize = ChunkSize;
+
+        _Carte = new Chunck[_Width / _ChunkSize, _Height / _ChunkSize];
+        for (int i = 0; i < (int)(_Width / _ChunkSize); i++)
+        {
+            for (int j = 0; j < (int)(_Height / _ChunkSize); j++)
+            {
+                _Carte[i, j] = new Chunck(_ChunkSize,_Depth);
+            }
+        }
+
+        for (int i = 0; i < _Width; i++)
+        {
+            this[i, _Height - 1,0] = new Full((i, _Height - 1), 1, 0, 0, 0, 0, 0);
+            this[i, 0,0] = new Full((i, 0), 1, 0, 0, 0, 0, 0);
+        }
+
+        for (int i = 0; i < _Height; i++)
+        {
+            this[_Width-1, i,0] = new Full((_Width-1,i), 1, 0, 0, 0, 0, 0);
+            this[0, i,0] = new Full((0, i), 1, 0, 0, 0, 0, 0);
+        }
+    }
+
+    public Box[] GetCollision(double x, double y)
+    {
+        List<Box> result = new List<Box>();
+        foreach (var wall in this[(int)x,(int)y])
+        {
+            if (wall.IsColliding((x, y)))
+            {
+                result.Add(wall);
+            }
+        }
+
+        return result.ToArray();
+    }
+
+    public int GetLength(int i)
+    {
+        if (i == 0)
+            return _Width;
+        
+        return _Height;
+    }
+
+    public void Save(string path)
+    {
+        using (BinaryWriter sw = new BinaryWriter(File.Open(path, FileMode.Create), Encoding.UTF8, false))
+            {
+                sw.Write(_ChunkSize);
+                sw.Write(_Width);
+                sw.Write(_Height);
+                sw.Write(_Depth);
+
+                foreach (var chunk in _Carte)
+                {
+                    chunk.Save(path, sw);
+                }
+            }
+
+    }
+
+    public void Load(string path)
+    {
+
+        if (File.Exists(path))
+        {
+            using (var stream = File.Open(path, FileMode.Open))
+            {
+                using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
+                {
+
+                    _ChunkSize = reader.ReadInt32();
+                    _Width = reader.ReadInt32();
+                    _Height = reader.ReadInt32();
+                    _Depth = reader.ReadInt32();
+
+                    _Carte = new Chunck[_Width / _ChunkSize, _Height / _ChunkSize];
+                    for (int i = 0; i < (int)(_Width / _ChunkSize); i++)
+                    {
+                        for (int j = 0; j < (int)(_Height / _ChunkSize); j++)
+                        {
+                            _Carte[i, j] = new Chunck(_ChunkSize, _Depth);
+                            _Carte[i,j].Read(path,_ChunkSize,reader, _Depth);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void MoveEntity(RaycastSprite Entity, (float X, float Y) Coord)
+    {
+        for (int i = -1; i < 2; i++)
+        {
+            for (int j = -1; j < 2; j++)
+            {
+                if (Entity.X + i >= 0 && Entity.X + i < _Width 
+                                      && Entity.Y + j >= 0 && Entity.Y + j < _Height)
+                    foreach (var value in this[(int)(Entity.X + i), (int)(Entity.Y + j)])
+                    {
+                        value._ContainsEntity = false;
+                    }
+            }
+        }
+        
+        for (int i = -1; i < 2; i++)
+        {
+            for (int j = -1; j < 2; j++)
+            {
+                if (Coord.X + i >= 0 && Coord.X + i < _Width
+                                     && Coord.Y + j >= 0 && Coord.Y + j < _Height)
+                    foreach (var value in this[(int)(Coord.X + i), (int)(Coord.Y + j)])
+                    {
+                        value._ContainsEntity = true;
+                    }
+            }
+        }
+
+        Entity.X = Coord.X;
+        Entity.Y = Coord.Y;
+    }
+
+    public override string ToString()
+    {
+        string result = "";
+        for (int i = 0; i < _Width; i++)
+        {
+            for (int j = 0; j < _Height; j++)
+            {
+                if (this[i, j] is Empty)
+                    result += '-';
+                else
+                    result += '#';
+            }
+
+            result += '\n';
+        }
+
+        return result;
+    }
+}
